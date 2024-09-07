cfb2c3d9d059359370b7db61f951a7b3f5d954b4
AL-Williams001 <arthurlynnwilliams01@gmail.com>
2024-09-07 16:37:35 +0800

    Output

---
+++ MyFirstProgram/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            Console.Write("Hey!");
+            Console.WriteLine("Hello!");
+
+            //This is a comment 
+            /*this
+             * is 
+             * a 
+             * multiline
+             * comment
+             */
+
+            Console.WriteLine("Arthur");
+           
+            Console.ReadLine();
+        }
+    }
+}
