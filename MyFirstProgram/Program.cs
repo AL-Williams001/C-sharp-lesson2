de512503c5dac4b4e352f66ac7ca9bb8d4b87fbb
AL-Williams001 <arthurlynnwilliams01@gmail.com>
2024-10-01 17:20:18 +0800

    abstract classes

--- MyFirstProgram/Program.cs
+++ MyFirstProgram/Program.cs
@@ -8,31 +8,20 @@ namespace MyFirstProgram
     {
         static void Main (string[] args)
         {
-            // inheritance = 1 or more child classes receiving fields, methods, etc. from a common parent  
+            // abstract classes = modifier that indicates missing components
+            //                    or incomplete implementation
 
             Car car = new Car();
             Bicycle bicycle = new Bicycle();
             Boat boat = new Boat();
-
-            Console.WriteLine(car.speed);
-            Console.WriteLine(car.wheels);
-            car.go();
-
-            Console.WriteLine(bicycle.speed);
-            Console.WriteLine(bicycle.wheels);
-            bicycle.go();
-
-            Console.WriteLine(boat.speed);
-            Console.WriteLine(boat.wheels);
-            boat.go();
-
+            //Vehicle vehicle = new Vehicle();
 
             Console.ReadLine();
         }
 
     }
 
-    class Vehicle
+    abstract class Vehicle
     {
         public int speed = 0;
 
@@ -44,18 +33,20 @@ namespace MyFirstProgram
     class Car : Vehicle
     {
         public int wheels = 4;
-       
+        int maxSpeed = 500;
         
     }
 
     class Bicycle : Vehicle
     {
         public int wheels = 2;
+        int maxSpeed = 50;
     }
 
     class Boat : Vehicle
     {
         public int wheels = 0;
+        int maxSpeed = 100;
     }
 
 
